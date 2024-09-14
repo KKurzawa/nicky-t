1e60aac3e93ce8c3a1df6fde4b476803d6d116ae
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-14 18:55:19 -0400

    updated colors

--- src/Components/AudioPlayer/AudioPlayer.jsx
+++ src/Components/AudioPlayer/AudioPlayer.jsx
@@ -1,8 +1,14 @@
 import './AudioPlayer.css'
+import { Spotify } from 'react-spotify-embed'
 
 const AudioPlayer = () => {
     return (
-        <div>AudioPlayer</div>
+        <article className='audio-player-main flex flex-col w-[80%] lg:gap-5'>
+            <h2 className='audio-title mb-6 lg:mb-1 text-2xl font-bold'>AUDIO</h2>
+            <Spotify width='100%' height='75%' link="https://open.spotify.com/artist/0YNmqwybMecHbvzJRdBtxs" />
+            <p className='spotify-note mt-2 lg:mt-0'>*Spotify player is for preview purposes. Please support Nicky T and The Snakecharmers and live music by purchasing merchandise on the audio page.</p>
+        </article>
+
     )
 }
 
--- src/Components/UpcomingShows/UpcomingShows.jsx
+++ src/Components/UpcomingShows/UpcomingShows.jsx
@@ -5,17 +5,13 @@ const UpcomingShows = () => {
     return (
         <article className='upcoming-shows-main w-[70%]'>
             <h2 className='upcoming-shows-title mb-1 text-2xl font-bold'>UPCOMING SHOWS</h2>
-            {/* <section className='date-shows grid grid-cols-2'>
-                <h3>Date</h3>
-                <h3>Show</h3>
-            </section> */}
             {showsList.map((item, index) => index < 5 && (
-                <article key={item.index} className='upcoming-shows-container flex flex-col gap-1 text-xl py-4'>
-                    <section className='upcoming-date-time flex gap-1'>
+                <article key={item.index} className='upcoming-shows-container flex flex-col lg:gap-1 md:text-base lg:text-xl py-4'>
+                    <section className='upcoming-date-time flex md:flex-col lg:flex-row lg:gap-1'>
                         <h4 className='font-bold'>{item.date}</h4>
                         <h4>@ {item.time}</h4>
                     </section>
-                    <section className='upcomint-venue-container flex flex-col gap-1'>
+                    <section className='upcomint-venue-container flex flex-col lg:gap-1'>
                         <button
                             className='upcoming-venue w-fit'
                             onClick={() => setTimeout(() => {
